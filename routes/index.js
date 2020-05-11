b94b66edc2c42984246a32276633cf5cc86ffa9c
yashodeep <yashodeepchikte@gmail.com>
2020-05-12 01:51:31 +0530

    all the routes are working and database is local

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const List = require("../models/list.model");
 const Item = require("../models/item.model");
-const db  = require("../config/key").mongoURI;
+
+const db  =  "mongodb://localhost/todolist"
 
 // Connecting the database
 mongoose.connect(db, { useUnifiedTopology: true,  useNewUrlParser: true})
-    .then(() => {console.log("monfo data base connected successfully")})
+    .then(() => {console.log("mongo data base connected successfully")})
     .catch(err => console.log(err))
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
